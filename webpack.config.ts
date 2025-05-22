22ab02b3e23496f054cc3556b13b31b031803ece
Sofia Deaza <sofiadeaza05@gmail.com>
2025-05-22 11:02:23 -0500

    scaffolding

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  mode: 'development',
+};
